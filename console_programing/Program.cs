eab8f125f98fd26e6d6607881d06b8237ab6c525
yagmurlu <mavibulut0106@gmail.com>
2021-12-18 18:49:26 +0300

    1.ders uygulaması

---
+++ console_programing/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace console_programing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string slm="Selam";
+           string isim=Console.ReadLine();
+           Console.WriteLine(slm+" "+ isim);
+           Console.ReadKey();
+        }
+    }
+}
